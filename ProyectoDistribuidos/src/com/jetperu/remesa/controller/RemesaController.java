0cab563119047805d786e393b1ef47a077f98c91
ronald.lopez.rufasto <ronald.lopez.rufasto@gmail.com>
2015-11-10 12:42:04 -0500

    Adaptar cliente con la validacion de regla de negocio

--- ProyectoDistribuidos/src/com/jetperu/remesa/controller/RemesaController.java
+++ ProyectoDistribuidos/src/com/jetperu/remesa/controller/RemesaController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.datacontract.schemas._2004._07.HoraExcedidaException;
 import org.datacontract.schemas._2004._07.Remesa;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.tempuri.IRemesaProxy;
+import org.tempuri.IRemesasProxy;
 
 import com.google.gson.Gson;
 import com.jetperu.remesa.bean.RemesaBean;
@@ -36,25 +37,27 @@ public class RemesaController {
 			HttpServletResponse response,
 			@RequestParam("numeroTransaccion") int numeroTransaccion) throws RemoteException {
 
-		IRemesaProxy iRemesaProxy = new IRemesaProxy();
-		Remesa remesa = iRemesaProxy.obtenerRemesa(numeroTransaccion);
-		
+		IRemesasProxy iRemesaProxy = new IRemesasProxy();
 		Map<String, Object> mapResultado = new HashMap<String, Object>();
 		int resultado = 0;
-		
 		RemesaBean remesaBean = null;
-		if (remesa != null) {
-			remesaBean = new RemesaBean();
-			remesaBean.setNumero(remesa.getNumero());
-			remesaBean.setPIN(remesa.getPIN());
-			remesaBean.setNombreAgencia(remesa.getAgencia().getNombre());
-			remesaBean.setNombreMoneda(remesa.getMoneda().getNombre());
-			remesaBean.setMonto(remesa.getMonto().doubleValue());
-			remesaBean.setEstado(remesa.getEstado());
-			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-			remesaBean.setFecha(dateFormat.format(remesa.getFecha().getTime()));
-		} else {
-			resultado = 2;
+		try {
+			Remesa remesa = iRemesaProxy.obtenerRemesa(numeroTransaccion);
+			if (remesa != null) {
+				remesaBean = new RemesaBean();
+				remesaBean.setNumero(remesa.getNumero());
+				remesaBean.setPIN(remesa.getPIN());
+				remesaBean.setNombreAgencia(remesa.getAgencia().getNombre());
+				remesaBean.setNombreMoneda(remesa.getMoneda().getNombre());
+				remesaBean.setMonto(remesa.getMonto().doubleValue());
+				remesaBean.setEstado(remesa.getEstado());
+				SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+				remesaBean.setFecha(dateFormat.format(remesa.getFecha().getTime()));
+			} else {
+				resultado = 2;
+			}
+		} catch (HoraExcedidaException e) {
+			resultado = 1;
 		}
 		
 		response.setContentType(MediaType.APPLICATION_JSON.toString());
