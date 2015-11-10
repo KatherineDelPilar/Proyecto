9da4b52d020e3307692b80c7496ab40b5111acbc
Ronald <Ronald@10.11.141.255>
2015-11-09 21:45:32 -0500

    Se crea cliente del servicio web

---
+++ ProyectoDistribuidos/src/org/tempuri/RemesaEjecutarOperacion.java
@@ -0,0 +1,16 @@
+/**
+ * RemesaEjecutarOperacion.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package org.tempuri;
+
+public interface RemesaEjecutarOperacion extends javax.xml.rpc.Service {
+    public java.lang.String getBasicHttpBinding_IRemesaAddress();
+
+    public org.tempuri.IRemesa getBasicHttpBinding_IRemesa() throws javax.xml.rpc.ServiceException;
+
+    public org.tempuri.IRemesa getBasicHttpBinding_IRemesa(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
